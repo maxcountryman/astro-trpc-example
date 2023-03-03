1abafbd833deb594849b3ee8bc69d84ef7b1d2f1
Max Countryman <maxc@me.com>
2023-03-03 15:29:14 -0800

    Update router.ts

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,8 +1,12 @@
 import { initTRPC } from '@trpc/server';
 import type { Context } from './context';
+import { z } from "zod";
 
 const t = initTRPC.context<Context>().create();
 export const appRouter = t.router({
   greeting: t.procedure.query(() => 'hello tRPC v10!'),
+  foo: t.procedure
+      .input(z.object({ x: z.string() }))
+      .mutation(({ input }) => input.x),
 });
 export type AppRouter = typeof appRouter;
